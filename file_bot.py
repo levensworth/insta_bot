3e50862d37f374d94c0c3b4276ae8a02af2869d6
santiagobassani96 <santiago.bassani96@gmail.com>
2018-02-03 15:43:10 -0300

    little mistake :)

--- file_bot.py
+++ file_bot.py
@@ -241,10 +241,8 @@ schedule.every(30).minutes.do(job_7)
 
 if __name__ == '__main__':
     setup(bot)
-    job_6()
 
-'''
+
     while True:
         schedule.run_pending()
         time.sleep(1)
-'''
